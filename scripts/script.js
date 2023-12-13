960dd7967558f2fa3b632d15b1807f6fa6db3c8e
andrewpopovic8 <145488880+andrewpopovic8@users.noreply.github.com>
2023-12-13 14:22:01 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
 /*
-    Student Name:
+    Student Name: Andrew Popovic
+    Date: 12/12/23
     File Name: script.js
-    Date: 
 */
+
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
