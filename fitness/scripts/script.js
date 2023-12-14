394b2f1d57477f78e6b900e75e98a72d56355c7a
andrewpopovic8 <145488880+andrewpopovic8@users.noreply.github.com>
2023-12-14 12:13:52 -0500

    Update script.js

--- fitness/scripts/script.js
+++ fitness/scripts/script.js
@@ -1,5 +1,54 @@
 /*
-    Student Name:
+    Student Name: Andrew Popovic
     File Name: script.js
-    Date: 
+    Date: 12/14/23
 */
+
+//Global Variables
+var video = document.getElementById("example");
+var videoSource = document.getElementById("vid-src");
+var descriptionSource = document.getElementById("despsrc");
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    var logo = document.getElementById("ffc-logo");
+    if (menu.style.display === "block" && logo.style.display === "none") {
+        menu.style.display = "none";
+        logo.style.display = "block";
+    }
+    else {
+        menu.style.display = "block";
+        logo.style.display = "none";
+    }
+}
+
+//Function to display the burpees example video
+function burpees() {
+    videoSource.src = "media/burpees.mp4";
+    descriptionSource.src = "media/burpees-descriptions.vvt";
+    video.style.display = "block";
+    video.load();
+}
+
+//Functions to display the plank example video
+function plank() {
+    videoSource.src = "media/plank.mp4";
+    descriptionSource.src = "media/plank-descriptions.vvt";
+    video.style.display = "block";
+    video.load();
+}
+//Functions to display the mountain climbers example video
+function mountain() {
+    videoSource.src = "media/mc.mp4";
+    descriptionSource.src = "media/mountain-descriptions.vvt";
+    video.style.display = "block";
+    video.load();
+}
+
+//Functions to display promo code
+function discount() {
+    var promo = document.getElementById("special");
+    promo.firstChild.nodeValue = "Promo Code: D25START";
+    promo.style.color = "#ff0000";
+    promo.style.fontSize = "2em";
+}
